46acf84019e2af40db1facab40a17f232296a65f
Patrick Thomson <patrick@helium.co>
2014-09-16 18:34:28 -0400

    tcp echo server

--- helium.h
+++ helium.h
@@ -1,3 +1,4 @@
 // Copyright (c) 2014 Helium Systems, Inc.
 
 const char* libhelium_version(void);
+
--- sender.c
+++ sender.c
@@ -37,11 +37,10 @@ void allocation_callback(uv_handle_t *handle, size_t size, uv_buf_t *buf)
 
 void connection_callback(uv_stream_t *server, int status)
 {
-  printf("In connection callback\n");
-  fflush(stdout);
-  uv_tcp_t *client = malloc(sizeof(uv_udp_t));
+  printf("In connection callback");
+  uv_udp_t *client = malloc(sizeof(uv_udp_t));
   assert(client != NULL);
-  uv_tcp_init(loop, client);
+  uv_udp_init(loop, client);
 
   int err = uv_accept(server, (uv_stream_t*)client);
   if (err) {
@@ -51,27 +50,44 @@ void connection_callback(uv_stream_t *server, int status)
   else {
     uv_read_start((uv_stream_t*)client, allocation_callback, reading_callback);
   }
+
+  
+  printf("In connection callback!\n");
 }
 
-void idle_callback(uv_idle_t *idler) {
-  
+void set_broadcast(uv_udp_t *send_socket) {
+  struct sockaddr_in broadcast_addr;
+
+  // The IP address 0.0.0.0 is used to bind to all interfaces.
+  // Port 0 means that the OS randomly assigns a port.
+  uv_ip4_addr("0.0.0.0", 0, &broadcast_addr);
+
+  //  init, bind, broadcast
+  uv_udp_init(loop, send_socket);
+  uv_udp_bind(send_socket, (const struct sockaddr*)&broadcast_addr, 0);
+  uv_udp_set_broadcast(send_socket, 1);
+}
+
+void send_callback(uv_udp_send_t *req, int status) {
+  printf("In send callback! status is %d\n", status);
 }
 
 int main(int argc, char *argv[])
 {
   loop = uv_default_loop();
   
-  uv_tcp_t server;
-  uv_tcp_init(loop, &server);
+  uv_udp_t server;
+  uv_udp_init(loop, &server);
 
   struct sockaddr_in bind_addr;
   int err = uv_ip4_addr("0.0.0.0", 40026, &bind_addr);
   assert(!err);
+  set_broadcast(&server);
 
-  err = uv_tcp_bind(&server, (struct sockaddr*)&bind_addr, 0);
-  assert(!err);
-
-  err = uv_listen((uv_stream_t *)&server, 128, connection_callback);
+  uv_buf_t to_send = uv_buf_init("wooo", 4);
+  
+  uv_udp_send_t send_request;
+  err = uv_udp_send(&send_request, &server, &to_send, 1, (struct sockaddr*)&bind_addr, send_callback);
   assert(!err);
   
   uv_run(loop, UV_RUN_DEFAULT);
