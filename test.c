747e47a9f1252ca874ccd484317a62a637f5274c
Patrick Thomson <patrick@helium.co>
2014-09-20 13:35:25 -0400

    enough trickery in CMakeLists to compile successfully with block support on clang and without on gcc. calling this a win

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 #include "helium_api.h"
 
 void test_callback(const helium_connection_t *conn, uint64_t sender_mac, char * const message, size_t count) {
-  printf("callback got %s %zd\n", message+1, count);
+  printf("Function pointer callback got %s %zd\n", message, count);
 }
 
 int main(int argc, char *argv[]) {
@@ -22,12 +22,17 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // helium_init(&conn, proxy, test_callback);
-
+#if HAVE_BLOCKS
   helium_init_b(&conn, proxy, ^(const helium_connection_t *conn, uint64_t mac, char *msg, size_t n) {
-      
+      printf("Block callback got %zu bytes from message %s", n, msg);
   });
+#else
+  helium_init(&conn, proxy, test_callback);
+#endif
 
+    
+  
+ 
   printf("blargh\n");
 
   char line[256];
